445739caaffa2c33df6720143d1fcce889ec340c
seembh <58399241+seembh@users.noreply.github.com>
2021-12-06 21:02:57 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,21 +3,20 @@ import subprocess
 
 
 users=[1,10,50,100,300,500,1000,1500]
-policies=[#'random',
-    #'round_robin',
-    #'reg_rr',
-    #'weight_rr',
+policies=['random',
+    'round_robin',
+    'reg_rr',
+    'weight_rr',
     'leastLatency',
     'leastConnection'
     ]
 
 def some_func():
     for i in range(len(policies)):
-        for j in range(6,7):
+        for j in range(len(users)):
             meanSum = 0
             for k in range(0,4):
                 string = "python3 lb.py -p " + policies[i] + " -n " + str(users[j])
-                #os.system(string)
                 timePolicy = float(subprocess.check_output(string, shell=True))
                 meanSum += timePolicy
 
@@ -26,6 +25,4 @@ def some_func():
             print(solution)
 
 if __name__ == '__main__':
-    # test1.py executed as script
-    # do something
     some_func()
